2c1008f82142504891eda2456d3c64b6fce191c9
aaese <st068075@student.spbu.ru>
2020-10-10 10:38:47 +0300

    улучшение

--- Project8/Project8/Source.cpp
+++ Project8/Project8/Source.cpp
@@ -7,33 +7,25 @@ int main() {
 	int l = 0;
 	int **A = new int*[n];
 	for (int i = 0; i<n; i++) {
-		A[i] = new int[n];
-	}
-
-	for (int i = 0; i < n; i++) {
+		A[i] = new int[m];
 		for (int j = 0; j < m; j++) {
 			A[i][j] = chisl;
 			chisl++;
-			if (i == j) {
-				sumD1 += A[i][j];
-			}
+			printf("%4d", A[i][j]);
 		}
+		cout << endl;
 	}
 	for (int i = (n-1); i >=0; i--) {
 		for (int j = (m-1); j >=0; j--) {
+			if (i == j) {
+				sumD1 += A[i][j];
+			}
 			if (i==k && j == l) {
 				sumD2 += A[i][j];
 				k--;
 				l++;
 			}
 		}
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			//cout << A[i][j];
-			printf("%4d", A[i][j]);
-		}
-		cout << endl;
 	}
 	cout << "Sum of glav diag: " << sumD1 << endl;
 	cout << "Sum of poboch diag: " << sumD2 << endl;
